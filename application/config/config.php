b3a8940466b94dc463ba62ac26f7180d8c1f2a62
Yabeh <abbsumarfs@gmail.com>
2020-04-23 21:54:11 +0700

    perbaikan controller

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/nf/tankerr-master/';
+$config['base_url'] = 'http://localhost/tankerr/';
 
 /*
 |--------------------------------------------------------------------------
